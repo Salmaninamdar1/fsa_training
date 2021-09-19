2b0dff19d5a7c921fadb0ae8d05d746539e26a12
Salman <salmaninamdar1@gmail.com>
2021-09-19 23:57:29 +0530

    finaly pblm sovled with help of this we can find prime no

--- problems.js
+++ problems.js
@@ -4,30 +4,28 @@
 // 4: 1,2,4
 // 16: 1 to 16, 4 count:3
 // %
-
+// second function
 function isFactor(n, i) {
     return n % i === 0;
 }
 
+// firstly exicute this function then
 function isPrime(n) {
     var count = 0;
     for (var i = 1; i <= n; i++) {
         if (isFactor(n, i)) count++;
+        // here we are passing parameter to the second fuction
     }
 
     return count === 2;
 
-    // if (count === 2) return true;
-    // return false;
-
+    // // if (count === 2) return true;
+    // // return false;
     // if (count === 2) return true;
     // else return false;
 }
-
-console.log("is n a prime no? ",isPrime(16));
-
-
+console.log("16 is a prime no? ",isPrime(16));
 var res2 = isPrime(5);
-console.log("is res2 is= ",res2);
+console.log("is 5 prime no ? = ",res2);
 // isPrime(1);  
 // isPrime(2);
